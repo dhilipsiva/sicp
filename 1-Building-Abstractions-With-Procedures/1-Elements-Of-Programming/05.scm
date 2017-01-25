8fe737d3e8bb07c7556e5a7c4f2582154c7fdc52
dhilipsiva <dhilipsiva@gmail.com>
2017-01-25 10:39:42 +0530

    Ex 1.5

---
+++ 1-Building-Abstractions-With-Procedures/1-Elements-Of-Programming/05.scm
@@ -0,0 +1,10 @@
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+	0
+	y))
+
+(test 0 (p))
+
+; In normal order, this is an endless recursive funtion
+; In Applicative order, this just returns (p)
