891b562f03a00b58c864ff6d3b89bf20c0381c9a
dhilipsiva <dhilipsiva@gmail.com>
2017-02-05 12:50:54 +0530

    Minor refactor

--- 1-Building-Abstractions-With-Procedures/1-Elements-Of-Programming/square-root.scm
+++ 1-Building-Abstractions-With-Procedures/1-Elements-Of-Programming/square-root.scm
@@ -4,23 +4,27 @@
 
 (define guess 1)
 
+(define (abssolute x)
+  (if (> x 0) x (- x)))
+
 (define (absolute-difference num1 num2)
-		(if (> num1 num2)
-			(- num1 num2)
-			(- num2 num1)))
+  (abssolute (-  num1 num2)))
+
+(define (average x y)
+  (/ (+ x y) 2))
 
 (define (improve-guess num guess)
-		(/ (+ guess (/ num guess)) 2))
+  (average guess (/ num guess)))
 
 (define (is-good-enough num guess)
-		(> accuracy (absolute-difference num (square guess))))
+  (> accuracy (absolute-difference num (square guess))))
 
 (define (newton-iter num guess)
-		(if (is-good-enough num guess)
-			guess
-			(newton-iter num (improve-guess num guess))))
+  (if (is-good-enough num guess)
+	guess
+	(newton-iter num (improve-guess num guess))))
 
 (define (square-root num)
-		(newton-iter num guess))
+  (newton-iter num guess))
 
 (square-root 4)
