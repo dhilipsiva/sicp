01cd4e58112ca49f3ed650b2478d276083ac696e
dhilipsiva <dhilipsiva@gmail.com>
2017-02-07 10:22:54 +0530

    Finally Figured out the answer

--- 1-Building-Abstractions-With-Procedures/2-Procedures-and-The-Processes-they-Generate/10-ackermann.scm
+++ 1-Building-Abstractions-With-Procedures/2-Procedures-and-The-Processes-they-Generate/10-ackermann.scm
@@ -16,3 +16,35 @@
 (define (g n) (A 1 n)) ; No Idea
 (define (h n) (A 2 n)) ; No Idea
 (define (k n) (* 5 n n)) ; This is just 5 * n^2
+
+
+(f 1) ; 2
+(f 2) ; 4
+(f 3) ; 6
+(f 4) ; 8
+; So this is, like I guessed, (* 2 n)
+
+(g 1) ; 2
+(g 2) ; 4
+(g 3) ; 8
+(g 4) ; 16
+; So this one is (^ 2 n)
+
+(h 1) ; 2     => 2
+(h 2) ; 4     => 2 ^ 2
+(h 3) ; 16    => 2 ^ 2 ^ 2
+(h 4) ; 65536 => 2 ^ 2 ^ 2 ^ 2
+
+(define (h n)
+  		(if (= n 1)
+  			2
+  			(^ (h (- n 1) 2)))) ; h(n) = h(n-1)^2 where n > 1 else 2
+
+
+(k 1) ; 5   => 5 * 1 ^ 2
+(k 2) ; 20  => 5 * 2 ^ 2
+(k 3) ; 45  => 5 * 3 ^ 2
+(k 4) ; 80  => 5 * 4 ^ 2
+
+(define (k n)
+		(* 5 (* n n))) ; k(n) = 5 * n^2
