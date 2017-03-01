8e269d7ab3dfe21fbfa881f22221be51a1a64f06
dhilipsiva <dhilipsiva@gmail.com>
2017-03-01 11:55:19 +0530

    Minor change

--- 1-Building-Abstractions-With-Procedures/2-Procedures-and-The-Processes-they-Generate/11-f-recursive-iterative.scm
+++ 1-Building-Abstractions-With-Procedures/2-Procedures-and-The-Processes-they-Generate/11-f-recursive-iterative.scm
@@ -11,20 +11,18 @@
 	   (* 2 (f-recursive (- n 2)))
 	   (* 3 (f-recursive (- n 3))))))
 
-(f-recursive 5)
+(f-recursive 3)
 
 (define (f-iter a b c count)
-   (if (< count 3)
-       a
-       (f-iter (+ a (* 2 b) (* 3 c))
-               a
-               b
-               (- count 1))))
+  (if (< count 3)
+	a
+	(f-iter (+ a (* 2 b) (* 3 c))
+			a
+			b
+			(- count 1))))
 
 (define (f-iterative n)
-   (if (< n 3)
-       n
-       (f-iter 2 1 0 n)))
+  (f-iter 2 1 0 n))
 
 
-(f-iterative 5)
+(f-iterative 3)
